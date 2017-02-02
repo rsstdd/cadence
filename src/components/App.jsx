bc82a1205eb4cf17a6ee4f58bf31d6995e4306f0
kculafic <kculafic@gmail.com>
2017-02-02 10:31:56 -0800

    Add App.js component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
+import Carousel from './Carousel'; 
 import React from 'react';
 
 class App extends React.Component {
   render() {
     return (
       <h1>Bob there</h1>
+      <div id="content">
+        <Carousel />
+      </div>
     )
   }
 }

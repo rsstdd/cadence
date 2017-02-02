47b8fb5f7911099fc213f8df007d62abc579a1c8
kculafic <kculafic@gmail.com>
2017-02-02 10:34:05 -0800

    Add Carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+import CarouselDots from './CarouselDots';
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  render() {
+    return (
+      <div className="carousel">
+        <div className="row">
+          <div className="one columns">◀︎</div>
+          <Card />
+          <div className="one columns">▶︎</div>
+        </div>
+        <div className="row carousel-dots">
+          <CarouselDots />
+        </div>
+      </div>
+    );
+  }
+}
